078fbcfdfdeaf250063d8fa40e42f2752c1c2fb0
Yaroslav Brahinets <y.brahinets@gmail.com>
2026-05-24 12:38:06 +0300

    Clean up old Go implementation

--- strava-booster/download/activity-page.go
+++
@@ -1,8 +0,0 @@
-package download
-
-type ActivitiesPage struct {
-	Activities []ActivityEntity `json:"models"`
-	Page       int              `json:"page"`
-	PerPage    int              `json:"perPage"`
-	Total      int              `json:"total"`
-}
--- strava-booster/go.mod
+++
@@ -1,3 +0,0 @@
-module strava-booster
-
-go 1.22.1
